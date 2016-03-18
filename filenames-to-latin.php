f141fc6d638bb4450cee52fbf210a50ee9cbc99e
Vitalii <vitalii.skorokhod@gmail.com>
2016-03-18 14:43:16 -0600

    Initial commit - ver.2.0

---
+++ filenames-to-latin.php
@@ -0,0 +1,176 @@
+<?php
+/*
+Plugin Name: Filenames to latin
+Plugin URI: http://wordpress.org/plugins/filenames-to-latin/
+Description: Sanitize Cyrillic, German, French, Polish, Spanish, Hungarian, Czech, Greek, Swedish and other filenames to latin during upload.
+Version: 2.0
+Author: webvitaly
+Author URI: http://web-profile.com.ua/wordpress/plugins/
+License: GPLv3
+*/
+
+
+if( ! function_exists( 'filenames_to_latin_unqprfx' ) ) :
+	function filenames_to_latin_unqprfx( $filename ) {
+
+		$chars_table = array(
+
+			// Cyrillic alphabet
+			'/А/' => 'a', '/Б/' => 'b', '/В/' => 'v', '/Г/' => 'g', '/Д/' => 'd',
+			'/а/' => 'a', '/б/' => 'b', '/в/' => 'v', '/г/' => 'g', '/д/' => 'd',
+
+			'/Е/' => 'e', '/Ж/' => 'zh', '/З/' => 'z', '/И/' => 'i', '/Й/' => 'j',
+			'/е/' => 'e', '/ж/' => 'zh', '/з/' => 'z', '/и/' => 'i', '/й/' => 'j',
+
+			'/К/' => 'k', '/Л/' => 'l', '/М/' => 'm', '/Н/' => 'n', '/О/' => 'o',
+			'/к/' => 'k', '/л/' => 'l', '/м/' => 'm', '/н/' => 'n', '/о/' => 'o',
+
+			'/П/' => 'p', '/Р/' => 'r', '/С/' => 's', '/Т/' => 't', '/У/' => 'u',
+			'/п/' => 'p', '/р/' => 'r', '/с/' => 's', '/т/' => 't', '/у/' => 'u',
+
+			'/Ф/' => 'f', '/Х/' => 'h', '/Ц/' => 'c', '/Ч/' => 'ch', '/Ш/' => 'sh',
+			'/ф/' => 'f', '/х/' => 'h', '/ц/' => 'c', '/ч/' => 'ch', '/ш/' => 'sh',
+
+			'/Щ/' => 'shch', '/Ь/' => '', '/Ю/' => 'ju', '/Я/' => 'ja',
+			'/щ/' => 'shch', '/ь/' => '', '/ю/' => 'ju', '/я/' => 'ja',
+
+			// Ukrainian
+			'/Ґ/' => 'g', '/Є/' => 'ye', '/І/' => 'i', '/Ї/' => 'yi',
+			'/ґ/' => 'g', '/є/' => 'ye', '/і/' => 'i', '/ї/' => 'yi',
+			
+			// Russian
+			'/Ё/' => 'yo', '/Ы/' => 'y', '/Ъ/' => '', '/Э/' => 'e',
+			'/ё/' => 'yo', '/ы/' => 'y', '/ъ/' => '', '/э/' => 'e',
+			
+			// Belorussian
+			'/Ў/' => 'u',
+			'/ў/' => 'u',
+
+			// German
+			'/Ä/' => 'ae', '/Ö/' => 'oe', '/Ü/' => 'ue', '/ß/' => 'ss',
+			'/ä/' => 'ae', '/ö/' => 'oe', '/ü/' => 'ue',
+			
+			// Polish
+			'/Ą/' => 'a', '/Ć/' => 'c', '/Ę/' => 'e', '/Ł/' => 'l', '/Ń/' => 'n',
+			'/ą/' => 'a', '/ć/' => 'c', '/ę/' => 'e', '/ł/' => 'l', '/ń/' => 'n',
+			'/Ó/' => 'o', '/Ś/' => 's', '/Ź/' => 'z', '/Ż/' => 'z',
+			'/ó/' => 'o', '/ś/' => 's', '/ź/' => 'z', '/ż/' => 'z',
+
+			// Hungarian
+			'/Ő/' => 'o', '/Ű/' => 'u',
+			'/ő/' => 'o', '/ű/' => 'u',
+
+			// Czech
+			'/Ě/' => 'e', '/Š/' => 's', '/Č/' => 'c', '/Ř/' => 'r', '/Ž/' => 'z',
+			'/ě/' => 'e', '/š/' => 's', '/č/' => 'c', '/ř/' => 'r', '/ž/' => 'z',
+
+			'/Ý/' => 'y', '/Á/' => 'a', '/É/' => 'e', '/Ď/' => 'd', '/Ť/' => 't',
+			'/ý/' => 'y', '/á/' => 'a', '/é/' => 'e', '/ď/' => 'd', '/ť/' => 't',
+
+			'/Ň/' => 'n', '/Ú/' => 'u', '/Ů/' => 'u',
+			'/ň/' => 'n', '/ú/' => 'u', '/ů/' => 'u',
+
+			// Greek alphabet & modern polytonic characters
+			'/Α/' => 'a', '/Β/' => 'v', '/Γ/' => 'g', '/Δ/' => 'd', '/Ε/' => 'e',
+			'/α/' => 'a', '/β/' => 'v', '/γ/' => 'g', '/δ/' => 'd', '/ε/' => 'e',
+
+			'/Ζ/' => 'z', '/Η/' => 'i', '/Θ/' => 'th', '/Ι/' => 'i', '/Κ/' => 'k',
+			'/ζ/' => 'z', '/η/' => 'i', '/θ/' => 'th', '/ι/' => 'i', '/κ/' => 'k',
+
+			'/Λ/' => 'l', '/Μ/' => 'm', '/Ν/' => 'n', '/Ξ/' => 'x', '/Ο/' => 'o',
+			'/λ/' => 'l', '/μ/' => 'm', '/ν/' => 'n', '/ξ/' => 'x', '/ο/' => 'o',
+
+			'/Π/' => 'p', '/Ρ/' => 'r', '/Σ/' => 's', '/Τ/' => 't', '/Υ/' => 'y',
+			'/π/' => 'p', '/ρ/' => 'r', '/σ/' => 's', '/τ/' => 't', '/υ/' => 'y',
+
+			'/Φ/' => 'f', '/Χ/' => 'ch', '/Ψ/' => 'ps', '/Ω/' => 'o', '/Ά/' => 'a',
+			'/φ/' => 'f', '/χ/' => 'ch', '/ψ/' => 'ps', '/ω/' => 'o', '/ά/' => 'a',
+
+			'/Έ/' => 'e', '/Ή/' => 'i', '/Ί/' => 'i', '/Ό/' => 'o', '/Ύ/' => 'y',
+			'/έ/' => 'e', '/ή/' => 'i', '/ί/' => 'i', '/ό/' => 'o', '/ύ/' => 'y',
+
+			'/Ώ/' => 'o', '/Ϊ/' => 'i', '/Ϋ/' => 'y',
+			'/ώ/' => 'o', '/ς/' => 's', '/ΐ/' => 'i', '/ϊ/' => 'i', '/ϋ/' => 'y', '/ΰ/' => 'y',
+
+			// Extra all (http://www.atm.ox.ac.uk/user/iwi/charmap.html)
+			'/À/' => 'a', '/Á/' => 'a', '/Â/' => 'a', '/Ã/' => 'a', '/Å/' => 'a',
+			'/à/' => 'a', '/á/' => 'a', '/â/' => 'a', '/ã/' => 'a', '/å/' => 'a',
+
+			'/Æ/' => 'ae', '/Ç/' => 'c', '/È/' => 'e', '/É/' => 'e', '/Ê/' => 'e',
+			'/æ/' => 'ae', '/ç/' => 'c', '/è/' => 'e', '/é/' => 'e', '/ê/' => 'e',
+
+			'/Ë/' => 'e', '/Ì/' => 'i', '/Í/' => 'i', '/Î/' => 'i', '/Ï/' => 'i',
+			'/ë/' => 'e', '/ì/' => 'i', '/í/' => 'i', '/î/' => 'i', '/ï/' => 'i',
+
+			'/Ð/' => 'd', '/Ñ/' => 'n', '/Ò/' => 'o', '/Ô/' => 'o', '/Õ/' => 'o',
+			'/ð/' => 'd', '/ñ/' => 'n', '/ò/' => 'o', '/ô/' => 'o', '/õ/' => 'o',
+
+			'/×/' => 'x', '/Ø/' => 'o', '/Ù/' => 'u', '/Ú/' => 'u', '/Û/' => 'u',
+			'/×/' => 'x', '/ø/' => 'o', '/ù/' => 'u', '/ú/' => 'u', '/û/' => 'u',
+
+			'/Þ/' => 'p', '/Ÿ/' => 'y',
+			'/þ/' => 'p', '/ÿ/' => 'y',
+
+			// Other
+			'/№/' => '', '/“/' => '', '/”/' => '', '/«/' => '', '/»/' => '',
+			'/„/' => '', '/@/' => '', '/%/' => '', '/‘/' => '', '/’/' => '',
+			'/`/' => '', '/´/' => '', '/º/' => 'o', '/ª/' => 'a',
+
+		);
+
+		// rewrite some chars for some languages
+		$locale = get_locale();
+		switch ( $locale ) {
+			case 'uk_UA': // Ukrainian
+			case 'uk_ua':
+			case 'uk':
+				$chars_table_ext = array(
+					'/Г/' => 'h',
+					'/г/' => 'h',
+					'/И/' => 'y',
+					'/и/' => 'y'
+				);
+				$chars_table = array_merge( $chars_table, $chars_table_ext );
+				break;
+			case 'sv_SE': // Swedish
+			case 'sv_se':
+				$chars_table_ext = array(
+					'/Å/' => 'a',
+					'/å/' => 'a',
+					'/Ä/' => 'a',
+					'/ä/' => 'a',
+					'/Ö/' => 'o',
+					'/ö/' => 'o'
+				);
+				$chars_table = array_merge( $chars_table, $chars_table_ext );
+				break;
+			case 'bg_BG': // Bulgarian
+			case 'bg_bg':
+				$chars_table_ext = array(
+					'/Щ/' => 'sht',
+					'/щ/' => 'sht',
+					'/Ъ/' => 'a',
+					'/ъ/' => 'a'
+				);
+				$chars_table = array_merge( $chars_table, $chars_table_ext );
+				break;
+		}
+
+		$friendly_filename = preg_replace( array_keys( $chars_table ), array_values( $chars_table ), $filename ); // replace original chars in filename with friendly chars
+
+		return strtolower( $friendly_filename ); // filename to lowercase
+	}
+	add_filter( 'sanitize_file_name', 'filenames_to_latin_unqprfx', 10 );
+endif;
+
+
+if( ! function_exists( 'filenames_to_latin_unqprfx_plugin_meta' ) ) :
+	function filenames_to_latin_unqprfx_plugin_meta( $links, $file ) { // add 'Plugin page' and 'Donate' links to plugin meta row
+		if ( strpos( $file, 'filenames-to-latin.php' ) !== false ) {
+			$links = array_merge( $links, array( '<a href="http://web-profile.com.ua/wordpress/plugins/filenames-to-latin/" title="Plugin page">' . __('Filenames to latin') . '</a>' ) );
+			$links = array_merge( $links, array( '<a href="http://web-profile.com.ua/donate/" title="Support the development">' . __('Donate') . '</a>' ) );
+		}
+		return $links;
+	}
+	add_filter( 'plugin_row_meta', 'filenames_to_latin_unqprfx_plugin_meta', 10, 2 );
+endif;
---
+++ readme.txt
@@ -0,0 +1,77 @@
+=== Filenames to latin ===
+Contributors: webvitaly
+Donate link: http://web-profile.com.ua/donate/
+Tags: filename, sanitize, media, upload, cyrillic, ukrainian, russian, belorussian, german, french, polish, spanish, hungarian, czech, greek
+Requires at least: 3.0
+Tested up to: 4.1
+Stable tag: 2.0
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl.html
+
+Sanitize filenames to latin during upload.
+
+== Description ==
+
+> **[WordPress Pro plugins](http://codecanyon.net/popular_item/by_category?category=wordpress&ref=webvitaly)** |
+> **[Filenames to latin](http://web-profile.com.ua/wordpress/plugins/filenames-to-latin/ "Plugin page")** |
+> **[Donate](http://web-profile.com.ua/donate/ "Support the development")**
+
+
+Plugin sanitizes cyrillic (Ukrainian, Russian, Belorussian), German, French, Polish, Spanish, Hungarian, Czech, Greek, Swedish and other filenames to latin characters during upload.
+Plugin converts filenames to lowercase automatically.
+
+= List of sanitizing letters: =
+а б в г д е ж з и й к л м н о п р с т у ф х ц ч ш щ ь ю я ґ є і ї ё ы ъ э ў ą ć ę ł ń ó ś ź ż à á â ã ä å æ ç è é ê ë ì í î ï ð ñ ò ó ô õ ö × ø ù ú û ü ý þ ÿ ß ő ű ě š č ř ž ý á é ď ť ň ú ů α β γ δ ε ζ η θ ι κ λ μ ν ξ ο π ρ σ τ υ φ χ ψ ω ά έ ή ί ό ύ ώ ς ΐ ϊ ϋ ΰ.
+
+
+= Useful: =
+* ["Anti-spam" - block spam in comments](http://wordpress.org/plugins/anti-spam/ "no spam, no captcha")
+* ["Page-list" - show list of pages with shortcodes](http://wordpress.org/plugins/page-list/ "list of pages with shortcodes")
+* [WordPress Pro plugins](http://codecanyon.net/popular_item/by_category?category=wordpress&ref=webvitaly)
+
+
+== Screenshots ==
+
+1. filenames converted without and with the plugin
+
+
+== Changelog ==
+
+= 2.0 - 2014-01-30 =
+* rewrite the plugin's code completely
+* combine 2 arrays of letters into 1 associative array for easier updating
+* add exceptions for some languages
+* added Swedish letters (thanks to Leah - http://wordpress.org/support/profile/gezan )
+
+= 1.8 - 2014-01-08 =
+* added Greek letters (thanks to senortiago)
+
+= 1.7 - 2013-07-24 =
+* minor changes
+
+= 1.6 - 2013-03-02 =
+* added Czech letters (thanks to Jiri)
+
+= 1.5 - 2013-01-13 =
+* fixed German letters (thanks to Bjoern)
+
+= 1.4 - 2012-10-31 =
+* minor changes
+
+= 1.3 =
+* added Hungarian letters
+
+= 1.2 =
+* converting filenames to lowercase automatically
+
+= 1.1 =
+* added Belorussian, German, Polish, Spanish and many other letters
+
+= 1.0 =
+* initial release
+
+
+== Installation ==
+
+1. Install plugin and activate it on the Plugins page
+2. Now your media filenames will be sanitized to latin automatically
